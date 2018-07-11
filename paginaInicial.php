3b87afaa69574c153f320dc2d0d17c6c7d0f3482
Nayara Oliveira <nayara_o@outlook.com>
2018-07-11 14:10:24 -0300

    Update paginaInicial.php
    Acrescentado os códigos de bootstrap e rodapé

--- paginaInicial.php
+++ paginaInicial.php
@@ -6,11 +6,14 @@ session_start();
 <head>
    <head>
     <meta charset="UTF-8"/>
-    
-    <link rel="stylesheet" type="text/css" href="css/CSSpagina.css">
-    <link rel="stylesheet" type="text/css" href="css/CSSpaginaInicial.css">
-    <link rel="shortcut icon" type="image/x-icon" href="imagens/favicon.ico">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+     <script src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
+          integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"
+            integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
+            crossorigin="anonymous"></script>
     <title>SejaChef</title>
     
     <script type="text/javascript">
@@ -107,12 +110,33 @@ session_start();
         </video>
     </div>
 
-    <div id="rodape">
-        <img id="imgrodape" src="imagens/img.sejaochef.png" align="center">
-        <b>CONTATOS: (82) 91111 - 2222 / sejachef@gmail.com</b>
+ <div class="container-fluid" id="rodape">
+        <div class=" container">
+            <div class="row">
+                <div class="col-xs-12 col-sn-12 col-md-4 col-lg-4">
+                    <img id="imgrodape" src="imagens/img.sejaochef.png" align="center">
+                </div>
+
+                <div class="col-xs-12 col-sn-12 col-md-4 col-lg-4">
+                    <p><span class=" glyphicon glyphicon-bishop"><b> CONTATOS: (82) 91111 - 2222</b></span></p>
+                </div>
+
+                <div class="col-xs-12 col-sn-12 col-md-4 col-lg-4">
+                    <p>&reg; copyright 2018 - Seja Chef</p>
+                </div>
+            </div>
+        </div>
     </div>
 
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"
+        integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
+        crossorigin="anonymous"></script>
 </div>
-
 </body>
 </html>
